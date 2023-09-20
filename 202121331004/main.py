0bcbb35b0a60dcc18b2f513b17e173ce9bdd1d36
Luokon <1427573460@qq.com>
2023-09-20 20:19:56 +0800

    Update Exception

--- 202121331004/main.py
+++ 202121331004/main.py
@@ -2,25 +2,32 @@ import gensim.corpora
 import jieba
 import re
 
+
 # 根据读入的文件路径读取文件,对文章进行分词并将标点符号过滤
 def deal_file(path):
     s = ""
-    with open(path, encoding='utf-8') as f:
-        line = f.readline()
-        while line != '':
-            s = s + line
+    # 读取文件
+    try:
+        with open(path, encoding='utf-8') as f:
             line = f.readline()
-    f.close()
+            while line != '':
+                s = s + line
+                line = f.readline()
+        f.close()
+    except Exception:
+        print("读入文件异常")
+
     # 分词
     s = jieba.cut(s)
-    ans = []
+    res = []
     # 通过正则表达式过滤标点符号
     for i in s:
         if re.match(u"[a-zA-Z0-9\u4e00-\u9fa5]", i) is not None:
-            ans.append(i)
+            res.append(i)
         else:
             pass
-    return ans
+    return res
+
 
 # 构造向量并计算余弦相似度
 def cosine_similarity(t1, t2):
@@ -29,11 +36,12 @@ def cosine_similarity(t1, t2):
     dictionary = gensim.corpora.Dictionary(txt)
     vec = [dictionary.doc2bow(t) for t in txt]
     # 计算余弦相似度
-    sim = gensim.similarities.Similarity('-Similarity-index', vec, num_features = len(dictionary))
+    sim = gensim.similarities.Similarity('-Similarity-index', vec, num_features=len(dictionary))
     corp1 = dictionary.doc2bow(t1)
     return sim[corp1][1]
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     print("请输入原论文的文件路径：")
     path1 = input()
     print("请输入抄袭论文的文件路径")
@@ -41,7 +49,7 @@ if __name__ == '__main__' :
 
     # path1 = "F:\\py_code\\Luokon\\202121331004\\test\\orig.txt"
     # path2 = "F:\\py_code\\Luokon\\202121331004\\test\\orig.add.txt"
-    res_path = "F:\\py_code\\Luokon\\202121331004\\test\\res.txt"
+    res_path = "F:\\py_code\\Luokon\\202121331004\\test\\r.txt"
 
     # 处理文章，返回过滤符号并分词的列表
     txt1 = deal_file(path1)
@@ -49,7 +57,11 @@ if __name__ == '__main__' :
 
     # 计算余弦相似度并写入结果文件
     ans = cosine_similarity(txt1, txt2)
-    print("两篇文章的相似度：%.2f" %ans)
-    file = open(res_path, 'w', encoding='utf-8')
-    file.write("两篇文章的相似度：%.2f" %ans)
-    file.close()
+    print("两篇文章的相似度：%.2f" % ans)
+    try:
+        file = open(res_path, 'w', encoding='utf-8')
+        file.write("两篇文章的相似度：%.2f" % ans)
+        file.close()
+    except Exception:
+        print("写入文件异常")
+
